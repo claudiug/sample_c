79b9c9a0c49d95950ae692312df878749b7963bc
claudiu <claudiu.garba@gmail.com>
2014-01-31 22:37:25 +0100

    fun

--- main.c
+++ main.c
@@ -8,9 +8,22 @@
 
 #include <stdio.h>
 
+void sayHi(char *student, char *course, int days){
+    printf("%s has done  as much: %s into this number of days: %d", student, course, days);
+}
+
 int main(int argc, const char * argv[])
 {
+    int i = 20;
     int j = 10;
-    int j = 10
+    int k = (i > j) ? 10 : 5;
+    if (5 < j - k) {
+        printf("I do not know");
+    } else if (j > i){
+        printf("maybe is possible");
+    }else{
+        printf("stupid");
+    }
+    sayHi("Claudiu", "Demo", 23);
 }
 
